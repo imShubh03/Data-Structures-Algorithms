88a7f9543ed714f43716fc1459b8c53c795aa053
Shubham7476 <shubhamsonake307@gmail.com>
2025-05-22 05:27:50 +0530

    array

--- Arrays/L1/group_Anagram.cpp
+++ Arrays/L1/group_Anagram.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*Brute TC: O(N* K logk)  sort str of len k SC:O(n)
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
@@ -21,6 +22,46 @@ public:
         return ans;
     }
 };
+*/
+
+// TC O(n * k) SC O(n * k)
+class Solution {
+    public:
+    string generateWord(string &s){
+        int arr[26] = {0};
+        for(char &ch: s)  {
+            arr[ch - 'a']++;
+        }
+
+        string newWrd = "";
+        for(int i = 0; i < 26; i++){
+            int freq = arr[i];
+            if(freq > 0){
+                newWrd += string(freq, i + 'a');
+            }
+        }
+        return newWrd;
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs){
+        int n = strs.size();
+        unordered_map<string, vector<string>> mp;
+        vector<vector<string>> ans;
+
+        for(int i = 0; i < n; i++){
+            string word = strs[i];
+            string newWord = generateWord(word);
+            mp[newWord].push_back(word);
+        }
+        
+        for(auto it : mp) {
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
+
 
 int main() {
     int n;
