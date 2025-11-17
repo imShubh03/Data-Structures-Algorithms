41175aa995fa94166376c23b6022597978f6d42f
Shubham <shubh.sonake17@gmail.com>
2025-11-17 23:49:01 +0530

    Refactor largestElement function to improve efficiency and readability

--- Arrays/L1/largesteleminarray.cpp
+++ Arrays/L1/largesteleminarray.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <vector>
 #include<algorithm>
-
-
 using namespace std;
 
 int largestElement(vector<int> &arr, int n) {
     sort(arr.begin(), arr.end());
-
-    int ans = arr[n-1];
-    return ans;
+    
+    for(int i = n-2; i>=0;i--){
+        if(arr[i] != arr[n-1]){
+            return arr[i];
+        }
+    }
+    
+    return -1;
 }
 
 /*
--- Arrays/L1/right-rotate-Arr-By-K-places.cpp
+++ Arrays/L1/right-rotate-Arr-By-K-places.cpp
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*TC:O(n) SC:O(n)
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        // [1, 2, 3, 4, 5] k = 3; n= 5
+        // [3, 4, 5, 1, 2]
+        int n = nums.size();
+        vector<int>temp(n);
+        k = k % n;
+
+        for(int i =0; i<k; i++){
+            temp[i] = nums[n - k + i];
+        }
+
+        for(int i = k; i < n; i++){
+            temp[i] = nums[i-k];
+        }
+
+        nums = temp;
+    }
+};
+*/
+
+// TC:O(n) SC:O(1)
 class Solution{
     public:
     void reverse(int arr[], int s, int e){
